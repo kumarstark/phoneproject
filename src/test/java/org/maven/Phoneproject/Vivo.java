82302cb9ca3ef81448c1037eccf5b11c60c72251
unknown <sathish4live@gmail.com>
2022-01-15 08:41:49 -0800

    phone

---
+++ src/test/java/org/maven/Phoneproject/Vivo.java
@@ -0,0 +1,15 @@
+package org.maven.Phoneproject;
+
+public class Vivo {
+public void v15() {
+	System.out.println("ravi");
+
+}
+	
+public static void main(String[] args) {
+	Vivo v = new Vivo();
+    v.v15();
+}	
+	
+	
+}
